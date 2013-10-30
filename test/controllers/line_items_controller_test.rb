b3fa17f02c7a760b92b2c8c15935bf52548dd36b
Robert Bornemann <bornemann.robert@yahoo.de>
2013-10-30 16:49:19 +0100

    bugfixes

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -18,13 +18,10 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post :create, line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id }
+      post :create, product_id: products(:ruby).id
     end
-    assert_redirected_to cart_path(assigns(:line_item).cart)
-  end
-
-    assert_redirected_to line_item_path(assigns(:line_item))
-  end
+      assert_redirected_to cart_path(assigns(:line_item).cart)
+    end
 
   test "should show line_item" do
     get :show, id: @line_item
@@ -45,8 +42,7 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_difference('LineItem.count', -1) do
       delete :destroy, id: @line_item
     end
-
-    assert_redirected_to line_items_path
-  end
-
+  end 
 end
+
+
