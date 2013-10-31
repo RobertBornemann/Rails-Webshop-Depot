413ea7259cd0936e478bf8673730e46131050dcc
Robert Bornemann <bornemann.robert@yahoo.de>
2013-10-31 16:54:40 +0100

    added -delete- button to the lineitem page

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -15,3 +15,15 @@ class Cart < ActiveRecord::Base
 		line_items.to_a.sum { |item| item.total_price }
 	end
 end
+
+
+# def add_product(product_id, product_price)
+#   current_item = line_items.where(:product_id => product_id).first
+# 	  if current_item
+# 	    current_item.quantity += 1
+# 	  else
+# 	    current_item = LineItem.new(:product_id => product_id, :price => product_price)
+# 	    line_items << current_item
+# 	  end
+#   current_item
+# end
