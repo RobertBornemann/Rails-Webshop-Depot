715eddc06080303e0e016f32d214ba9335a28704
Robert Bornemann <bornemann.robert@yahoo.de>
2013-11-06 14:42:55 +0100

    migration that copies the product price into the line item

---
+++ db/migrate/20131106095933_update_price_in_line_item.rb
@@ -0,0 +1,17 @@
+class UpdatePriceInLineItem < ActiveRecord::Migration
+  def up
+  	LineItem.all.each do |item|
+  		item.price = item.product.price
+  		item.save!
+  	end
+  end
+
+	def down
+		LineItem.all.each do |item|
+			item.price = nil
+			item.save!
+		end
+	end
+	
+end
+
